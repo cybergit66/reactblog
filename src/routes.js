9c9ed1a2f29a949e31f1c15dc1193595926e91e8
Terence Highsmith <thighsmi@mssda16050503.msfc.nasa.gov>
2016-12-15 16:55:29 -0600

    added routes

--- src/routes.js
+++ src/routes.js
@@ -2,23 +2,14 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
-const Greeting = () => {
-    return <div>Hello there</div>;
-};
 
-const Greeting2 = () => {
-    return <div>Goodbye</div>;
-};
-
-const Greeting3 = () => {
-    return <div>Hello Again</div>;
-};
 
 export default (
     <Route path="/" component={App} >
-        <Route path="greet" component={Greeting} />
-        <Route path="greet2" component={Greeting2} />
-        <Route path="greet3" component={Greeting3} />
+        <IndexRoute component={PostsIndex} />
+        <Route path="posts/new" component={PostsNew} />
     </Route>
 );
